2265dc139d38018f57cf99f30a16a3ee3c6815aa
Christopher Imrie <christopher.imrie@cloudspecialist.uk>
2023-11-24 00:43:18 +0000

    test: Fixes failing package utility test

--- src/utils/package.test.ts
+++ src/utils/package.test.ts
@@ -3,7 +3,7 @@ import { getPackageProperty } from "./package";
 describe("Package utils", () => {
     describe("#getPackageProperty", () => {
         it("should return the property value if it exists", () => {
-            expect(getPackageProperty("name")).toEqual("aws-cdk-constructs");
+            expect(getPackageProperty("name")).toEqual("@release-gateway/aws-cdk-constructs");
         });
         it("should return undefined if the property does not exist", () => {
             expect(getPackageProperty("doesnt exist" as "name")).toEqual(undefined);
