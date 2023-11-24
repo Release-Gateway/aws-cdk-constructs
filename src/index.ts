c47bd30a0da35a518f235acb7141d95b4550d36d
Christopher Imrie <christopher.imrie@cloudspecialist.uk>
2023-11-24 01:38:11 +0000

    fix: Adds typescript typings and cleans up npm module

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./RGApp";
+export * from "./RGGuardValidator";
+export * from "./RGLogGroup";
+export * from "./RGNodejsFunction";
+export * from "./RGQueue";
+export * from "./RGStack";
